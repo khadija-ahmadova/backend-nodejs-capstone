d7fdfe7c02cb63f179ed7b6ce2157e4d8a2a4bc3
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-03-22 13:21:59 -0400

    implement delete endpoint

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -108,10 +108,15 @@ router.put('/:id', async(req, res,next) => {
 // Delete an existing item
 router.delete('/:id', async(req, res,next) => {
     try {
-        //Step 6: task 1 - insert code here
-        //Step 6: task 2 - insert code here
-        //Step 6: task 3 - insert code here
-        //Step 6: task 4 - insert code here
+        const db = await connectToDatabase();
+        const collection = db.collection("secondChanceItems");
+        const secondChanceItem = await collection.findOne({ id });
+        if (!secondChanceItem) {
+        logger.error('secondChanceItem not found');
+        return res.status(404).json({ error: "secondChanceItem not found" });
+        }
+        await collection.deleteOne({ id });
+        res.json({"deleted":"success"});
     } catch (e) {
         next(e);
     }
